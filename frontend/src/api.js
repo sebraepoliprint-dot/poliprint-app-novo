e56563c66503d87cca1c5cd415bc8e16138b55fb
sebraepoliprint-dot <sebrae.poliprint@gmail.com>
2026-09-23 17:29:38 -0400

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const BASE = process.env.EXPO_PUBLIC_BACKEND_URL;
+const BASE = https://poliprint-app-novo.onrender.com](https://poliprint-app-novo.onrender.com)";
 export const API = `${BASE}/api`;
 
 const client = axios.create({ baseURL: API });
